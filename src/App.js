3d235537048b0699ca6963e7bdb155c1200801e9
Rupesh Kumar <rkchourasia2001@gmail.com>
2023-04-10 16:56:34 +0530

    responsive added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@
 import React from "react";
 import './App.css';
 import LandingPage from './Components/LandingPage';
-import Home from "./Components/Home/Home";
-import Template from "./Components/Template";
-import ResumeFormat2 from "./Components/Resume/ResumeFormat2";
+
 
 function App() {
   return (
     <div className="main__container">
-
       <LandingPage  />
-      {/* <Home /> */}
     </div>
   );
 }
